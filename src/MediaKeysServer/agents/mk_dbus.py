66a5c84e7365ec2f08e7250d958f7321ec872cae
jldupont <github@jldupont.com>
2011-08-09 05:57:38 -0400

    initial

---
+++ src/MediaKeysServer/agents/mk_dbus.py
@@ -0,0 +1,76 @@
+"""
+    MediaKeys Dbus Agent
+    
+    Messages Generated:
+    - "mk_key_press" (key, source, priority)
+        priority: 1 -> low, 5 -> high
+    
+    Created on 2010-10-22
+    @author: jldupont
+"""
+import dbus.service
+    
+from MediaKeysServer.system.base import AgentThreadedBase
+from MediaKeysServer.system import mswitch
+
+__all__=[]
+
+
+class MKSignalRx1(dbus.service.Object):
+    """ works under ubuntu < 10.10
+    """
+    PATH=None
+    
+    def __init__(self, agent):
+        dbus.service.Object.__init__(self, dbus.SystemBus(), self.PATH) ## not sure we need this just to receive signals...
+        self.agent=agent
+        
+        dbus.SystemBus().add_signal_receiver(self.sCondition,
+                                       signal_name="Condition",
+                                       dbus_interface="org.freedesktop.Hal.Device",
+                                       bus_name=None,
+                                       path=None
+                                       )            
+    def sCondition(self, *p):
+        """
+        DBus signal handler
+        """
+        if len(p) == 2:
+            if (p[0]=="ButtonPressed"):
+                mswitch.publish(self.agent, "mk_key_press", p[1], "source1", 5)
+
+class MKSignalRx2(dbus.service.Object):
+    """ works on ubuntu >= 10.10
+    """
+    PATH=None #"/org/gnome/SettingsDaemon/MediaKeys"
+    
+    def __init__(self, agent):
+        dbus.service.Object.__init__(self, dbus.SessionBus(), self.PATH) ## not sure we need this just to receive signals...
+        self.agent=agent
+        
+        dbus.SessionBus().add_signal_receiver(self.sCondition,
+                                       signal_name="MediaPlayerKeyPressed",
+                                       dbus_interface="org.gnome.SettingsDaemon.MediaKeys",
+                                       bus_name=None,
+                                       path="/org/gnome/SettingsDaemon/MediaKeys"
+                                       )            
+    def sCondition(self, *p):
+        """
+        DBus signal handler
+        """
+        if len(p) == 2:
+            mswitch.publish(self.agent, "mk_key_press", p[1].lower(), "source2", 1)
+
+
+
+class DbusAgent(AgentThreadedBase):
+    
+    def __init__(self):
+        AgentThreadedBase.__init__(self)
+
+        self.srx1=MKSignalRx1(self)
+        self.srx2=MKSignalRx2(self)
+        
+                   
+_=DbusAgent()
+_.start()
---
+++ src/MediaKeysServer/scripts/main.py
@@ -0,0 +1,63 @@
+'''
+Created on 2011-08-09
+
+@author: jldupont
+'''   
+import sys
+
+APP_VERSION="0.1"
+APP_NAME="MediaKeysServer"
+ICON_NAME="mediakeysserver.png"
+HELP_URL="http://www.systemical.com/doc/opensource/mediakeysserver"
+TIME_BASE=5000
+
+###<<< DEVELOPMENT MODE SWITCHES
+MSWITCH_OBSERVE_MODE=False
+MSWITCH_DEBUGGING_MODE=False
+MSWITCH_DEBUG_INTEREST=False
+DEV_MODE=True
+###>>>
+
+import MediaKeysServer.system.setup #@UnusedImport
+from   MediaKeysServer.system import base as base
+from   MediaKeysServer.system import mswitch #@UnusedImport
+
+base.debug=DEV_MODE
+base.debug_interest=MSWITCH_DEBUG_INTEREST
+
+mswitch.observe_mode=MSWITCH_OBSERVE_MODE
+mswitch.debugging_mode=MSWITCH_DEBUGGING_MODE
+
+def main(debug=False):
+    try:
+        
+        from   MediaKeysServer.res import get_res_path
+        from   MediaKeysServer.agents.squeeze_tray import TrayAgent
+        import MediaKeysServer.agents.mk_dbus #@UnusedImport
+        import MediaKeysServer.agents.squeeze #@UnusedImport
+        from   MediaKeysServer.agents.clock import Clock #@Reimport        
+        from   MediaKeysServer.agents.notifier import notify, NotifierAgent #@Reimport
+                
+        icon_path=get_res_path()
+        _ta=TrayAgent(APP_NAME, icon_path, ICON_NAME, HELP_URL, APP_VERSION)
+
+        _na=NotifierAgent(APP_NAME, ICON_NAME)
+        _na.start()
+
+        _clk=Clock(TIME_BASE)
+        _clk.init()
+        
+        mswitch.publish("__main__", "debug", debug)
+        
+        import gtk
+        gtk.main()
+        
+    except KeyboardInterrupt:
+        mswitch.quit()
+        sys.exit(1)        
+        
+    except Exception,e:
+        notify(APP_NAME, "There was an error: %s" % e)
+        mswitch.quit()
+        sys.exit(1)
+
