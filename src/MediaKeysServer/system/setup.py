a5a80031c06da9d2dd765155925362bb4b590248
jldupont <github@jldupont.com>
2011-08-11 21:44:52 -0400

    app

--- src/MediaKeysServer/system/setup.py
+++ src/MediaKeysServer/system/setup.py
@@ -3,10 +3,13 @@ Created on 2011-03-30
 
 @author: jldupont
 '''
-import gobject
-import dbus.glib
-from dbus.mainloop.glib import DBusGMainLoop
+import util
 
-gobject.threads_init()  #@UndefinedVariable
-dbus.glib.init_threads()
-DBusGMainLoop(set_as_default=True)
+if util.isLinux():
+    import gobject
+    import dbus.glib
+    from dbus.mainloop.glib import DBusGMainLoop
+    
+    gobject.threads_init()  #@UndefinedVariable
+    dbus.glib.init_threads()
+    DBusGMainLoop(set_as_default=True)
