4ee401e8977e42740ce289a414a59e8d5c5214f6
jldupont <github@jldupont.com>
2011-08-13 11:22:03 -0400

    0.1

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+'''
+Created on 2011-05-05
+
+@author: jldupont
+'''
+
+__author__  ="Jean-Lou Dupont"
+__version__ ="0.1"
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name=         'mediakeysserver',
+      version=      __version__,
+      description=  'Media Keys websocket server',
+      author=       __author__,
+      author_email= 'jl@jldupont.com',
+      url=          'http://www.systemical.com/doc/opensource',
+      package_dir=  {'': "src",},
+      packages=     find_packages("src"),
+      scripts=      ['src/scripts/mediakeysserver',
+                     ],
+      package_data = {
+                      '':[ "*.gif", "*.png", "*.jpg", "*.desktop" ],
+                      },
+      include_package_data=True,                      
+      zip_safe=False
+      )
