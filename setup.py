563f9a30217bb888a8310e7716f01ce942b1c908
jldupont <github@jldupont.com>
2011-08-13 11:39:39 -0400

    0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Created on 2011-05-05
 '''
 
 __author__  ="Jean-Lou Dupont"
-__version__ ="0.1"
+__version__ ="0.2"
 
 from distutils.core import setup
 from setuptools import find_packages
@@ -17,12 +17,20 @@ setup(name=         'mediakeysserver',
       author=       __author__,
       author_email= 'jl@jldupont.com',
       url=          'http://www.systemical.com/doc/opensource',
-      package_dir=  {'': "src",},
-      packages=     find_packages("src"),
+      packages=     ["MediaKeysServer", 
+                     "MediaKeysServer.agents", 
+                     "MediaKeysServer.res",
+                     "MediaKeysServer.scripts",
+                     "MediaKeysServer.system"
+                     ],
+      package_dir=  {'MediaKeysServer': "src/MediaKeysServer",},
       scripts=      ['src/scripts/mediakeysserver',
                      ],
       package_data = {
-                      '':[ "*.gif", "*.png", "*.jpg", "*.desktop" ],
+                      '':[ "*.gif", 
+                            "**.png", 
+                            "*.jpg", 
+                            "*.desktop" ],
                       },
       include_package_data=True,                      
       zip_safe=False
